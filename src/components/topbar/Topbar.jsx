b87b170552f13cc08392821ba76dcc866795916b
leandrakell <leandrakell@gmail.com>
2022-10-08 20:24:40 -0300

    feat: add topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import './topbar.css';
+
+import { Chat, Notifications, Person, Search } from '@mui/icons-material';
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">KellySocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search className='searchIcon' />
+          <input
+            placeholder="Search for friends, posts or any videos"
+            type="text"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
